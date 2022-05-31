acbabce97fd2bcd7b3162d5b0a6b7ab594aef7c0
nicolasabosch <nicolasabosch@gmail.com>
2022-05-31 13:40:11 -0300

    Add project files.

---
+++ Ejercicios c++ parte 2.cpp
@@ -0,0 +1,188 @@
+#include <iostream>
+#pragma warning(disable : 4996)
+#include <math.h>
+using namespace std;
+
+//20)
+//int operacionSuma()
+//{
+//	int a;
+//	int b;
+//	printf("Cual es el valor de A ?");
+//	scanf_s("%i", &a);
+//	printf("Cual es el valor de B ?");
+//	scanf_s("%i", &b);
+//	int res;
+//	res = (a + b);
+//	printf("%i", res);
+//	return res;
+//}
+//int operacionResta()
+//{
+//	int a;
+//	int b;
+//	printf("Cual es el valor de A ?");
+//	scanf_s("%i", &a);
+//	printf("Cual es el valor de B ?");
+//	scanf_s("%i", &b);
+//
+//	int res;
+//	res = (a - b);
+//	printf("%i", res);
+//	return res;
+//	
+//}
+//
+//int operacionMulti()
+//{
+//	int a;
+//	int b;
+//	printf("Cual es el valor de A ?");
+//	scanf_s("%i", &a);
+//	printf("Cual es el valor de B ?");
+//	scanf_s("%i", &b);
+//
+//	int res;
+//	res = (a * b);
+//	printf("%i", res);
+//	return res;
+//}
+//int operacionDivi()
+//{
+//	int a;
+//	int b;
+//	printf("Cual es el valor de A ?");
+//	scanf_s("%i", &a);
+//	printf("Cual es el valor de B ?");
+//	scanf_s("%i", &b);
+//
+//	int res;
+//	res = (a / b);
+//	printf("%i", res);
+//	return res;
+//}
+
+int main()
+{
+	/*
+	13)
+	int num;
+	 printf("Ingresa un numero y te devuelvo sus divisores positivos\n");
+
+	 scanf("%i", &num);
+
+	 for (int i=1; i < num; i++) {
+
+		 if (num%i==0)
+		 {
+			 printf("%i es divisor de %i\n", i, num);
+		 }
+
+	 }
+	 */
+
+	 /*
+	  int num;
+
+	  printf("Ingresa un numero\n");
+	  scanf_s("%i", &num);
+
+
+	  for (int i = 0; i < num; i++)
+	  {
+		  printf_s("%i", i);
+	  }
+	  */
+
+	  /*
+	  15)
+	  for (int i = 1; i < 100; i++) {
+		  printf("%i", i);
+	  }
+	  */
+
+	  /*
+	  16)
+	  for (int i = 1; i < 100; i++) {
+
+		  if (i % 2 == 0)
+		  {
+			  printf("%i\n", i);
+		  }
+
+	  }
+	  */
+
+	  /*
+	  17)
+	  int num;
+	 printf("Decime un numero pa");
+
+	 scanf("%i", &num);
+
+	 for (int i = 0; i < num; i++)
+	 {
+		 printf("%i\n", i);
+	 }*/
+	 /*
+	 for (int i = 44; i < 72; i++) {
+		 if (i % 4 == 0) {
+			 printf("%i\n", i);
+		 }
+	 */
+
+
+
+	 /*
+	 19)
+	 double num = 4.0;
+
+	 for (double i = 1;i <= num;i++)
+
+	 {
+		 double x = 1;
+
+		 for (double j = 1; j <= i;j++) {
+
+			 x = x * 10;
+
+		 }
+
+		 printf("%.1f \n", x);
+	 }*/
+
+
+	/*
+	20)
+	int caso;
+	printf("Que caso elegis?\n");
+	scanf_s("%i", &caso);
+
+	switch (caso)
+	{
+	case 1:
+
+		operacionSuma();
+		
+
+		break;
+	case 2:
+
+		operacionResta();
+		
+		
+		break;
+		
+	case 3:
+
+		operacionMulti();
+
+		break;
+	case 4:
+
+		operacionDivi();
+
+		break;
+
+	}*/
+}
